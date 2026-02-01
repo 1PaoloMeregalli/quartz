e0350b04c2dbf7aaaca0198896e30a089ed7b3cf
paolo meregalli <paolomeregalli@MacBookPro.lan>
2026-02-01 15:12:22 +0100

    vault backup: 2026-02-01 15:12:21

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,8 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "www.paolomeregalli.it",
+/*    baseUrl: "quartz.jzhao.xyz", */
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
