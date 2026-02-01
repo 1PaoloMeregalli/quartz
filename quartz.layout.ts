a5cc6915dcd3648e8a73954116b8ef919f1fd932
paolo meregalli <paolomeregalli@MacBookPro.lan>
2026-02-01 18:54:23 +0100

    vault backup: 2026-02-01 18:54:23

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -45,7 +45,7 @@ export const defaultContentPageLayout: PageLayout = {
       component: Component.Graph({
         localGraph: false, // false = mostra tutto il sito (effetto galassia)
         showTags: true, //AGGIUNTO IO
-        linkDistance: 5, //AGGIUNTO IO
+        linkDistance: 2, //AGGIUNTO IO
         depth: -1 , // Profondità infinita
       }),
       condition: (page) => page.fileData.slug === "index",
