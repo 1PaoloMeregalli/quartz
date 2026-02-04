e950b39d90b3e44dc4b0fefc6e79c09153809252
paolo meregalli <paolomeregalli@MacBookPro.lan>
2026-02-04 07:34:25 +0100

    vault backup: 2026-02-04 07:34:25

--- quartz.config.ts
+++ quartz.config.ts
@@ -88,7 +88,6 @@ const config: QuartzConfig = {
       Plugin.Assets(),
       Plugin.Static(),
       Plugin.Favicon(),
-      Plugin.Graph(), //AGGIUNTO IO
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
