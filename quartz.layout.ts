fa09d99eb7be8a8cfae8f4839c2b35a9b296343a
paolo meregalli <paolomeregalli@paolos-MacBook-Pro-2.local>
2026-02-12 20:25:45 +0100

    vault backup: 2026-02-12 20:25:45

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -97,7 +97,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
-
+}
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
