4298d77cd7c6363cb596f9bd921b484ffa58c5b5
paolo meregalli <paolomeregalli@paolos-MacBook-Pro-2.local>
2026-02-14 07:53:02 +0100

    vault backup: 2026-02-14 07:53:02

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -55,7 +55,7 @@ export const defaultContentPageLayout: PageLayout = {
           depth: 1,
           // leggibilità “tipo Obsidian”
           focusOnHover: false,
-          scale: 1.8,      // <-- QUESTO ti avvicina
+          scale: 3,      // <-- QUESTO ti avvicina
           fontSize: 1.5,   // <-- QUESTO rende leggibili le etichette
           opacityScale: 15,
           showTags: true,
@@ -69,7 +69,7 @@ export const defaultContentPageLayout: PageLayout = {
           zoom: true,
           depth: -1,
           focusOnHover: false,   // <<--- IMPORTANTISSIMO
-          scale: 1.8,      // opzionale: anche il globale più leggibile
+          scale: 3,      // opzionale: anche il globale più leggibile
           fontSize: 1.0,
           opacityScale: 15,
           showTags: true,
